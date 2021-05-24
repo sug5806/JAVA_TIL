a254e417f133e8a17533cb82a0364ca14182db92
홍동수 <sug5806@naver.com>
2021-05-24 13:50:20 +0900

    stream 추가

---
+++ stream/src/input_stream/file_input_stream/FileInputStreamTest.java
@@ -0,0 +1,18 @@
+package input_stream.file_input_stream;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class FileInputStreamTest {
+    public static void main(String[] args) throws FileNotFoundException {
+        // FileInputStream을 만드는 방법 2가지
+
+        // 첫번째 방법
+        FileInputStream fileInputStream = new FileInputStream("src/stream/input_stream/file_input_stream/origin_test.txt");
+
+        // 두번째 방법
+        File file = new File("src/stream/input_stream/file_input_stream/origin_test.txt");
+        FileInputStream fileInputStream1 = new FileInputStream(file);
+    }
+}
---
+++ stream/src/reader/file_reader/CsvFileTest.java
@@ -0,0 +1,22 @@
+package reader.file_reader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class CsvFileTest {
+    public static void main(String[] args) throws IOException {
+        FileReader fr = new FileReader("src/stream/reader/file_reader/csv_file_test.csv");
+        BufferedReader br = new BufferedReader(fr);
+
+        String line = "";
+
+        while ((line = br.readLine()) != null) {
+            System.out.println("line = " + line);
+        }
+        br.close();
+        fr.close();
+
+    }
+}
