691a95db66b84972ec9cf6ff9edc28263bb6f3ef
홍동수 <sug5806@naver.com>
2021-05-29 15:04:01 +0900

    각종 테스트 추가

--- test_refactoring/src/string_calculator/StringCalculator.java
+++ test_refactoring/src/string_calculator/StringCalculator.java
@@ -1,31 +1,51 @@
 package string_calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
-    public int add(String str) {
-        String[] numbers = stringSplit(str);
+
+    public int add(String text) {
+        String[] numbers;
+
+        if (text == null) {
+            return 0;
+        } else if (text.equals("")) {
+            return 0;
+        } else if (text.startsWith("/")) {
+            numbers = customSeparatorSplit(text);
+        } else {
+            numbers = separatorSplit(text);
+        }
+
         int result = 0;
+
         for (String number : numbers) {
+            validator(number);
             result = result + Integer.parseInt(number);
         }
+
         return result;
     }
 
-    public int sub(String str) {
-        String[] numbers = stringSplit(str);
-        return Integer.parseInt(numbers[0]) - Integer.parseInt(numbers[1]);
+    private String[] customSeparatorSplit(String text) {
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(text);
+
+        if (matcher.find()) {
+            String separator = matcher.group(1);
+            return matcher.group(2).split(separator);
+        }
+
+        return null;
     }
 
-    public int divide(String str) {
-        String[] numbers = stringSplit(str);
-        return Integer.parseInt(numbers[0]) / Integer.parseInt(numbers[1]);
+    private String[] separatorSplit(String text) {
+        return text.split(",|;");
     }
 
-    public int multiple(String str) {
-        String[] numbers = stringSplit(str);
-        return Integer.parseInt(numbers[0]) * Integer.parseInt(numbers[1]);
-    }
-
-    private String[] stringSplit(String str) {
-        return str.split(",|;");
+    private void validator(String num) {
+        if (Integer.parseInt(num) < 0) {
+            throw new RuntimeException("숫자는 음수일 수 없습니다.");
+        }
     }
 }
--- test_refactoring/test/string_calculator/StringCalculatorTest.java
+++ test_refactoring/test/string_calculator/StringCalculatorTest.java
@@ -1,5 +1,6 @@
 package string_calculator;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,44 +18,8 @@ class StringCalculatorTest {
     @Test
     @DisplayName("더하기 테스트")
     public void addTest() throws Exception {
-        // given
-        // when
         int result = stringCalculator.add("1,2");
-        // then
         assertEquals(3, result);
-    }
-
-    @Test
-    @DisplayName("빼기 테스트")
-    public void subTest() throws Exception {
-        // given
-        int result = stringCalculator.sub("1,2");
-        // when
-
-        // then
-        assertEquals(-1, result);
-    }
-
-    @Test
-    @DisplayName("나눗셈 테스트")
-    public void divideTest() throws Exception {
-        // given
-        int result = stringCalculator.divide("5,2");
-        // when
-
-        // then
-        assertEquals(2, result);
-    }
-
-    @Test
-    @DisplayName("곱하기 테스트")
-    public void multipleTest() throws Exception {
-        // given
-        int result = stringCalculator.multiple("3,4");
-        // when
-
-        // then
-        assertEquals(12, result);
     }
 
     @Test
@@ -74,6 +39,28 @@ class StringCalculatorTest {
     @Test
     public void 콜론_또는_쉼표_구분자() {
         assertEquals(10, stringCalculator.add("2,3;5"));
+    }
+
+    @Test
+    public void 커스텀_구분자() {
+        assertEquals(8, stringCalculator.add("//z\n5z3"));
+    }
+
+    @Test
+    public void 음수일때() {
+        Assertions.assertThrows(RuntimeException.class, () ->
+                stringCalculator.add("1;-2")
+        );
+    }
+
+    @Test
+    public void null_입력() {
+        assertEquals(0, stringCalculator.add(null));
+    }
+
+    @Test
+    public void 빈_문자열_입력() {
+        assertEquals(0, stringCalculator.add(""));
     }
 
 }
