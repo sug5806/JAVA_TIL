8210b5e60f5541e96eebb5035aa79c455a8c48a1
홍동수 <sug5806@naver.com>
2021-05-31 15:22:26 +0900

    RequestLine 클래스 추가 RequestLineTest 코드 추가

---
+++ web_application_server/src/test/util/RequestLineTest.java
@@ -0,0 +1,33 @@
+package util;
+
+import org.junit.jupiter.api.Test;
+import webserver.RequestLine;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RequestLineTest {
+
+    @Test
+    public void create_method() {
+        RequestLine requestLine = new RequestLine("GET /index.html HTTP/1.1");
+        assertThat("GET").isEqualTo(requestLine.getMethod());
+        assertThat("/index.html").isEqualTo(requestLine.getPath());
+
+        requestLine = new RequestLine("POST /index.html HTTP/1.1");
+        assertThat("POST").isEqualTo(requestLine.getMethod());
+        assertThat("/index.html").isEqualTo(requestLine.getPath());
+    }
+
+    @Test
+    public void create_path_and_params() {
+        RequestLine requestLine = new RequestLine("GET /user/create?userId=gildong&password=1234 HTTP/1.1");
+        assertThat("GET").isEqualTo(requestLine.getMethod());
+        assertThat("/user/create").isEqualTo(requestLine.getPath());
+
+        Map<String, String> params = requestLine.getParams();
+        assertThat(2).isEqualTo(params.size());
+    }
+
+}
