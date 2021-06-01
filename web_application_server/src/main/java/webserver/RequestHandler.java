319ef09c33dea8602ba5cec7205c87072f0ba123
홍동수 <sug5806@naver.com>
2021-06-01 10:14:51 +0900

    RequestHandler 메서드 분리

--- web_application_server/src/main/java/webserver/RequestHandler.java
+++ web_application_server/src/main/java/webserver/RequestHandler.java
@@ -30,48 +30,15 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             HttpRequest request = new HttpRequest(in);
             HttpResponse response = new HttpResponse(out);
+
             String path = getDefaultUrl(request.getPath());
 
             if ("/user/create".equals(path)) {
-
-                User user = new User(
-                        request.getParameter("userId"),
-                        request.getParameter("password"),
-                        request.getParameter("name"),
-                        request.getParameter("email"));
-
-                log.debug("user : {}", user);
-                DataBase.addUser(user);
-                response.sendRedirect("/index.html");
+                createUser(request, response);
             } else if ("/user/login".equals(path)) {
-                User user = DataBase.findUserById(request.getParameter("userId"));
-                if (user != null) {
-                    if (user.getPassword().equals(request.getParameter("password"))) {
-                        response.addHeader("Set-Cookie", "logined=true");
-                    } else {
-                        response.sendRedirect("/index.html");
-                    }
-                } else {
-                    response.sendRedirect("/user/login_failed.html");
-                }
+                login(request, response);
             } else if ("/user/list".equals(path)) {
-                if (!isLogin(request.getHeader("Cookie"))) {
-                    response.sendRedirect("/user/login.html");
-                    return;
-                }
-
-                Collection<User> users = DataBase.findAll();
-                StringBuilder sb = new StringBuilder();
-                sb.append("<table border='1'>");
-                for (User user : users) {
-                    sb.append("<tr>");
-                    sb.append("<td>" + user.getUserId() + "</td>");
-                    sb.append("<td>" + user.getName() + "</td>");
-                    sb.append("<td>" + user.getEmail() + "</td>");
-                    sb.append("</tr>");
-                }
-                sb.append("</table>");
-                response.forward(sb.toString());
+                listUser(request, response);
             } else {
                 response.forward(path);
             }
@@ -97,5 +64,49 @@ public class RequestHandler extends Thread {
         return path;
     }
 
+    private void createUser(HttpRequest request, HttpResponse response) {
+        User user = new User(
+                request.getParameter("userId"),
+                request.getParameter("password"),
+                request.getParameter("name"),
+                request.getParameter("email"));
+
+        log.debug("user : {}", user);
+        DataBase.addUser(user);
+        response.sendRedirect("/index.html");
+    }
+
+    private void login(HttpRequest request, HttpResponse response) {
+        User user = DataBase.findUserById(request.getParameter("userId"));
+        if (user != null) {
+            if (user.getPassword().equals(request.getParameter("password"))) {
+                response.addHeader("Set-Cookie", "logined=true");
+            } else {
+                response.sendRedirect("/index.html");
+            }
+        } else {
+            response.sendRedirect("/user/login_failed.html");
+        }
+    }
+
+    private void listUser(HttpRequest request, HttpResponse response) {
+        if (!isLogin(request.getHeader("Cookie"))) {
+            response.sendRedirect("/user/login.html");
+            return;
+        }
+
+        Collection<User> users = DataBase.findAll();
+        StringBuilder sb = new StringBuilder();
+        sb.append("<table border='1'>");
+        for (User user : users) {
+            sb.append("<tr>");
+            sb.append("<td>").append(user.getUserId()).append("</td>");
+            sb.append("<td>").append(user.getName()).append("</td>");
+            sb.append("<td>").append(user.getEmail()).append("</td>");
+            sb.append("</tr>");
+        }
+        sb.append("</table>");
+        response.forward(sb.toString());
+    }
 
 }
