62672cbaa15736f37b1cfae771068005f9fe9606
홍동수 <sug5806@naver.com>
2021-06-08 17:43:40 +0900

    CreateUserController 리팩토링
    메모리 DataBase -> UserDao사용 변경

--- slipp/src/main/java/next/controller/CreateUserController.java
+++ slipp/src/main/java/next/controller/CreateUserController.java
@@ -1,13 +1,14 @@
 package next.controller;
 
-import core.db.DataBase;
 import core.mvc.Controller;
+import next.dao.UserDao;
 import next.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.SQLException;
 
 public class CreateUserController implements Controller {
     private static final Logger log = LoggerFactory.getLogger(CreateUserController.class);
@@ -18,7 +19,14 @@ public class CreateUserController implements Controller {
                 req.getParameter("email"));
         log.debug("User : {}", user);
 
-        DataBase.addUser(user);
+        UserDao userDao = new UserDao();
+
+        try {
+            userDao.insert(user);
+        } catch (SQLException e) {
+            log.error(e.getMessage());
+        }
+
         return "redirect:/";
     }
 }
