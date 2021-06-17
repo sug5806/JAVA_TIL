4e610f01b14a5d5f3460ed830f0b78006c0fbe6e
홍동수 <sug5806@naver.com>
2021-06-17 23:13:48 +0900

    백준 온라인 저지 2588번 문제 풀이

---
+++ boj_algorithm/src/input_output/number11/Main.java
@@ -0,0 +1,32 @@
+package input_output.number11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(bufferedReader.readLine());
+        int B = Integer.parseInt(bufferedReader.readLine());
+
+        bufferedReader.close();
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+
+        stringBuilder.append(A * (B % 10));
+        stringBuilder.append("\n");
+
+        stringBuilder.append(A * (B / 10 % 10));
+        stringBuilder.append("\n");
+
+        stringBuilder.append(A * (B / 100));
+        stringBuilder.append("\n");
+
+        stringBuilder.append(A * B);
+
+        System.out.println(stringBuilder);
+    }
+}
