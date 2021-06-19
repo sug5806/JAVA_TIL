ea961f3acecb057b7d2697fdd0dd6e61a04524d3
홍동수 <sug5806@naver.com>
2021-06-19 11:56:17 +0900

    백준 온라인 저지 14681번 문제 풀이

---
+++ boj_algorithm/src/if_/Q_14681/Main.java
@@ -0,0 +1,27 @@
+package if_.Q_14681;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String valueX = bufferedReader.readLine().trim();
+        String valueY = bufferedReader.readLine().trim();
+
+        int x = Integer.parseInt(valueX);
+        int y = Integer.parseInt(valueY);
+
+        if (x > 0 && y > 0) {
+            System.out.println(1);
+        } else if (x < 0 && y > 0) {
+            System.out.println(2);
+        } else if (x < 0 && y < 0) {
+            System.out.println(3);
+        } else {
+            System.out.println(4);
+        }
+    }
+}
