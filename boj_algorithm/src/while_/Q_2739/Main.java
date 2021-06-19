f80328acd9667bd2f82b791e86d5d2a1d329f417
홍동수 <sug5806@naver.com>
2021-06-19 12:12:52 +0900

    백준 온라인 저지 2739번 문제 풀이

---
+++ boj_algorithm/src/while_/Q_2739/Main.java
@@ -0,0 +1,22 @@
+package while_.Q_2739;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = bufferedReader.readLine().trim();
+
+        int n = Integer.parseInt(s);
+
+        int index = 1;
+
+        while (index <= 9) {
+            System.out.printf("%d * %d = %d\n", n, index, n * index);
+            index++;
+        }
+    }
+}
