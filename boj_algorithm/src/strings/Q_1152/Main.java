e6f8609f7ac085d848280e6943a4774e4b20fa0e
홍동수 <sug5806@naver.com>
2021-06-20 11:01:39 +0900

    백준 온라인 저지 1152번 문제 풀이

---
+++ boj_algorithm/src/strings/Q_1152/Main.java
@@ -0,0 +1,18 @@
+package strings.Q_1152;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        StringTokenizer stringTokenizer = new StringTokenizer(s, " ");
+
+        System.out.println(stringTokenizer.countTokens());
+    }
+}
