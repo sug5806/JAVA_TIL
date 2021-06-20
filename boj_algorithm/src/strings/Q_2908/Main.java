b59d42b5e5ff111b627c8a443c8e732c5a3fb2b9
홍동수 <sug5806@naver.com>
2021-06-20 11:10:15 +0900

    백준 온라인 저지 2908번 문제 풀이

---
+++ boj_algorithm/src/strings/Q_2908/Main.java
@@ -0,0 +1,30 @@
+package strings.Q_2908;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        String s = br.readLine();
+
+        stringBuilder.append(s);
+        stringBuilder.reverse();
+
+        StringTokenizer stringTokenizer = new StringTokenizer(stringBuilder.toString(), " ");
+
+        int num1 = Integer.parseInt(stringTokenizer.nextToken());
+        int num2 = Integer.parseInt(stringTokenizer.nextToken());
+
+        if (num1 < num2) {
+            System.out.println(num2);
+        } else {
+            System.out.println(num1);
+        }
+    }
+}
