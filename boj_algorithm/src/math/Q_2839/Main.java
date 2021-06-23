4a268dc816fc017cdadcdb1dfc50b7602c8d791a
홍동수 <sug5806@naver.com>
2021-06-23 23:40:15 +0900

    백준 온라인 저지 2839번 문제 풀이
    표를 만들어 풀면 풀만하다

---
+++ boj_algorithm/src/math/Q_2839/Main.java
@@ -0,0 +1,24 @@
+package math.Q_2839;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 설탕은 3kg, 5kg 봉지만 있다
+        int N = Integer.parseInt(br.readLine());
+
+        if (N == 4 || N == 7) {
+            System.out.println(-1);
+        } else if (N % 5 == 0) {
+            System.out.println(N / 5);
+        } else if (N % 5 == 1 || N % 5 == 3) {
+            System.out.println((N / 5) + 1);
+        } else if (N % 5 == 2 || N % 5 == 4) {
+            System.out.println((N / 5) + 2);
+        }
+    }
+}
