4763e72e0dcca183672090c6a4faa6a2f83ee131
홍동수 <sug5806@naver.com>
2021-07-01 23:22:13 +0900

    백준 온라인저지 2581번 문제 풀이

---
+++ boj_algorithm/src/math2/Q_2581/Main.java
@@ -0,0 +1,56 @@
+package math2.Q_2581;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static boolean[] prime;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        prime = new boolean[N + 1];
+        get_prime();
+
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = M; i <= N; i++) {
+            if (!prime[i]) {
+                sum += i;
+                if (min == Integer.MAX_VALUE) {
+                    min = i;
+                }
+            }
+        }
+
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+
+    }
+
+    public static void get_prime() {
+        prime[0] = true;
+        prime[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if (prime[i]) {
+                continue;
+            }
+
+            for (int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+
+        }
+    }
+}
