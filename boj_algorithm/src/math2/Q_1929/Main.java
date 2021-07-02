72ec71c7b9551e4a3e00ef8c8b1355667eefb2af
홍동수 <sug5806@naver.com>
2021-07-02 23:14:29 +0900

    백준 온라인저지 1929번 문제 풀이

---
+++ boj_algorithm/src/math2/Q_1929/Main.java
@@ -0,0 +1,47 @@
+package math2.Q_1929;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static boolean[] prime;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        prime = new boolean[N + 1];
+        get_prime();
+
+        for (int i = M; i <= N; i++) {
+            if (prime[i] == false) {
+                sb.append(i).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+
+    private static void get_prime() {
+        prime[0] = true;
+        prime[1] = true;
+
+        for (int i = 2; i < Math.sqrt(prime.length); i++) {
+            if (prime[i]) continue;
+
+            for (int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
+
+        }
+    }
+}
