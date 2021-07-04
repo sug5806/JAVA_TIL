7195cf770c3bf58cd76edec4e25093fecdb759ce
홍동수 <sug5806@naver.com>
2021-07-04 22:24:14 +0900

    사용자마다 사용하는 DB가 다를 수 있으므로 Connection을 추상 메서드로 만들어 사용자마다 다르게 구현하도록 변경

--- oop/src/main/java/dao/UserDao.java
+++ oop/src/main/java/dao/UserDao.java
@@ -2,9 +2,12 @@ package dao;
 
 import domain.User;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class UserDao {
+public abstract class UserDao {
     public void add(User user) throws ClassNotFoundException, SQLException {
         Connection c = getConnection();
 
@@ -44,12 +47,5 @@ public class UserDao {
         return user;
     }
 
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost/springbook", "spring", "book"
-        );
-
-        return c;
-    }
+    public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
 }
