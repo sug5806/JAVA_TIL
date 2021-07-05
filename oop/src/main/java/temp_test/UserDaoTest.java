0901d567e73cff2339cc844dd1a2848a6957fe2f
홍동수 <sug5806@naver.com>
2021-07-05 15:20:54 +0900

    커넥션 카운터를 알고 싶다면 새로운 클래스를 만들어 의존관계를 설정하여 해결할 수 있다.

--- oop/src/main/java/temp_test/UserDaoTest.java
+++ oop/src/main/java/temp_test/UserDaoTest.java
@@ -11,5 +11,8 @@ public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         UserDao dao = context.getBean("userDao", UserDao.class);
+        UserDao dao2 = context.getBean("userDao", UserDao.class);
+
+        System.out.println(dao2 == dao);
     }
 }
