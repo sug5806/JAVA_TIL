e9bad805761f6c9c2bcaf2a68f1bc0f35a2ed357
홍동수 <sug5806@naver.com>
2021-07-05 14:12:22 +0900

    Spring을 적용

--- oop/src/main/java/factory/DaoFactory.java
+++ oop/src/main/java/factory/DaoFactory.java
@@ -3,13 +3,19 @@ package factory;
 import connection.ConnectionMaker;
 import connection.DConnectionMaker;
 import dao.UserDao;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class DaoFactory {
+
+    @Bean
     public UserDao userDao() {
         ConnectionMaker connectionMaker = connectionMaker();
         return new UserDao(connectionMaker);
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
--- oop/src/main/java/temp_test/UserDaoTest.java
+++ oop/src/main/java/temp_test/UserDaoTest.java
@@ -2,11 +2,14 @@ package temp_test;
 
 import dao.UserDao;
 import factory.DaoFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
     }
 }
