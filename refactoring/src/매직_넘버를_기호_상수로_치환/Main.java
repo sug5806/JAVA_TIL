2ca41d18f2b928383a6919e4ac345abab9d0df8a
홍동수 <sug5806@naver.com>
2021-07-14 10:01:55 +0900

    매직 넘버를 기호 상수로 치환
    매직 넘버를 사용하여 의미를 알기 어렵고 여러곳에 있으면 변경하기 어렵다.

---
+++ refactoring/src/매직_넘버를_기호_상수로_치환/Main.java
@@ -0,0 +1,11 @@
+package 매직_넘버를_기호_상수로_치환;
+
+public class Main {
+    public static void main(String[] args) {
+        Robot robot = new Robot("Andrew");
+
+        robot.order(0);
+        robot.order(1);
+        robot.order(2);
+    }
+}
