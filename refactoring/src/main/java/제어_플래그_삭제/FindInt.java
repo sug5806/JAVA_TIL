cdfaa7e7d32fea73f1c190d50b2d8f4bcdf1305c
홍동수 <sug5806@naver.com>
2021-07-15 09:20:00 +0900

    return 사용으로 변경

--- refactoring/src/main/java/제어_플래그_삭제/FindInt.java
+++ refactoring/src/main/java/제어_플래그_삭제/FindInt.java
@@ -2,16 +2,13 @@ package 제어_플래그_삭제;
 
 public class FindInt {
     public static boolean find(int[] data, int target) {
-        boolean found = false;
-
         for (int i = 0; i < data.length; i++) {
             if (data[i] == target) {
-                found = true;
-                break;
+                return true;
             }
         }
 
-        return found;
+        return false;
 
     }
 }
