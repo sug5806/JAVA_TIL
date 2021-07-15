1d570f4d953a4996f79f49b5129c48fc21eef41d
홍동수 <sug5806@naver.com>
2021-07-15 09:18:19 +0900

    제어 플래그 삭제 리팩토링 전
    플래그 사용으로 코드가 복잡해졌다

---
+++ refactoring/src/main/java/제어_플래그_삭제/FindInt.java
@@ -0,0 +1,16 @@
+package 제어_플래그_삭제;
+
+public class FindInt {
+    public static boolean find(int[] data, int target) {
+        boolean flag = false;
+
+        for (int i = 0; i < data.length && !flag; i++) {
+            if (data[i] == target) {
+                flag = true;
+            }
+        }
+
+        return flag;
+
+    }
+}
---
+++ refactoring/src/main/java/제어_플래그_삭제/Main.java
@@ -0,0 +1,15 @@
+package 제어_플래그_삭제;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] data = {
+                1, 9, 0, 2, 8, 5, 6, 3, 4, 7,
+        };
+
+        if (FindInt.find(data, 5)) {
+            System.out.println("Found");
+        } else {
+            System.out.println("Not found...");
+        }
+    }
+}
