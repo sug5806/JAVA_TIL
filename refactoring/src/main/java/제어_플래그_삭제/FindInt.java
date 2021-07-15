8565815a3a60f66ecc6c428d1b02ff89f9e8d737
홍동수 <sug5806@naver.com>
2021-07-15 09:19:29 +0900

    변수명 변경 및 break사용으로 리팩토링
    flag -> found
    break 사용

--- refactoring/src/main/java/제어_플래그_삭제/FindInt.java
+++ refactoring/src/main/java/제어_플래그_삭제/FindInt.java
@@ -2,15 +2,16 @@ package 제어_플래그_삭제;
 
 public class FindInt {
     public static boolean find(int[] data, int target) {
-        boolean flag = false;
+        boolean found = false;
 
-        for (int i = 0; i < data.length && !flag; i++) {
+        for (int i = 0; i < data.length; i++) {
             if (data[i] == target) {
-                flag = true;
+                found = true;
+                break;
             }
         }
 
-        return flag;
+        return found;
 
     }
 }
