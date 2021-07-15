92c331c7805c8523150947ee98205808d82ecded
홍동수 <sug5806@naver.com>
2021-07-15 10:36:30 +0900

    널 객체 도입
    Person 클래스에 null을 확인하는 코드가 너무 많다

---
+++ refactoring/src/test/java/널_객체_도입/before/Label.java
@@ -0,0 +1,17 @@
+package 널_객체_도입.before;
+
+public class Label {
+    private final String _label;
+
+    public Label(String label) {
+        _label = label;
+    }
+
+    public void display() {
+        System.out.println("display: " + _label);
+    }
+
+    public String toString() {
+        return "\"" + _label + "\"";
+    }
+}
