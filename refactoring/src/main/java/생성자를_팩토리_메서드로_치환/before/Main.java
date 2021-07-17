05230177356f0214906369a70aea692ed831ee95
홍동수 <sug5806@naver.com>
2021-07-17 23:44:58 +0900

    생성자를 팩토리 메서드로 치환
    Main에서 new로 object를 만드는대신 팩토리 메서드 호출

--- refactoring/src/main/java/생성자를_팩토리_메서드로_치환/before/Main.java
+++ refactoring/src/main/java/생성자를_팩토리_메서드로_치환/before/Main.java
@@ -2,9 +2,9 @@ package 생성자를_팩토리_메서드로_치환.before;
 
 public class Main {
     public static void main(String[] args) {
-        Shape line = new Shape(Shape.TYPECODE_LINE, 0, 0, 100, 200);
-        Shape rectangle = new Shape(Shape.TYPECODE_RECTANGLE, 10, 20, 30, 40);
-        Shape oval = new Shape(Shape.TYPECODE_OVAL, 100, 200, 300, 400);
+        Shape line = Shape.create(Shape.TYPECODE_LINE, 0, 0, 100, 200);
+        Shape rectangle = Shape.create(Shape.TYPECODE_RECTANGLE, 10, 20, 30, 40);
+        Shape oval = Shape.create(Shape.TYPECODE_OVAL, 100, 200, 300, 400);
 
         Shape[] shape = {
                 line,
