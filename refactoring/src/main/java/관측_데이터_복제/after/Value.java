ea35cf4ae665fd6feb3e6764c37469c802d5fcb6
홍동수 <sug5806@naver.com>
2021-07-18 00:25:18 +0900

    관측 데이터 복제
    IntegerDisplay 클래스 내부에 _value를 Value 클래스로 변경

---
+++ refactoring/src/main/java/관측_데이터_복제/after/Value.java
@@ -0,0 +1,19 @@
+package 관측_데이터_복제.after;
+
+public class Value {
+    private int _value = 0;
+
+    public Value(int value) {
+        _value = value;
+    }
+
+    public int getValue() {
+        return _value;
+    }
+
+    public void setValue(int value) {
+        _value = value;
+    }
+
+
+}
