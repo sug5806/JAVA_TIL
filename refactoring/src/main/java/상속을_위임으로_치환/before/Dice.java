98568b70de8b48585657a602cb052cc6e74080e1
홍동수 <sug5806@naver.com>
2021-07-18 10:22:43 +0900

    상속을 위임으로 치환
    Random 클래스를 상속받은 Dice클래스가 Random클래스의 nextInt 메서드만 오버라이드하고 나머지 메서드는 상속 거부.
    이는 계약 위반이므로 상속을 사용하면 안된다.

---
+++ refactoring/src/main/java/상속을_위임으로_치환/before/Dice.java
@@ -0,0 +1,48 @@
+package 상속을_위임으로_치환.before;
+
+import java.util.Random;
+
+public class Dice extends Random {
+    public Dice() {
+        super(314159L);
+    }
+
+    public Dice(long seed) {
+        super(seed);
+    }
+
+    @Override
+    public int nextInt() {
+        return nextInt(6) + 1;
+    }
+
+    @Override
+    public void nextBytes(byte[] bytes) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public long nextLong() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean nextBoolean() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public float nextFloat() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public double nextDouble() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public synchronized double nextGaussian() {
+        throw new UnsupportedOperationException();
+    }
+}
