8a96f8c5f6bf5ae4787e58f498f2c22693cbb217
홍동수 <sug5806@naver.com>
2021-07-18 10:25:30 +0900

    상속을 위임으로 치환
    1. 위임용 필드 도입
      (1). 하위 클래스에 상위 클래스 타입의 위임용 필드 선언
      (2). 생성자 안에서 위임용 필드를 this로 초기화
      (3). 상속받던 메서드를 재작성해서 위임용 필드를 이용하게 함
      (4). 컴파일해서 테스트

---
+++ refactoring/src/main/java/상속을_위임으로_치환/after/Dice.java
@@ -0,0 +1,52 @@
+package 상속을_위임으로_치환.after;
+
+import java.util.Random;
+
+public class Dice extends Random {
+    private final Random _random;
+
+    public Dice() {
+        super(314159L);
+        _random = this;
+    }
+
+    public Dice(long seed) {
+        super(seed);
+        _random = this;
+    }
+
+    @Override
+    public int nextInt() {
+        return _random.nextInt(6) + 1;
+    }
+
+    @Override
+    public void nextBytes(byte[] bytes) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public long nextLong() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean nextBoolean() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public float nextFloat() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public double nextDouble() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public synchronized double nextGaussian() {
+        throw new UnsupportedOperationException();
+    }
+}
